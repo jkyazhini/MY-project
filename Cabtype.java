81dfb3d381dc452e27679a09705fc5c48d9a7e6e
jkyazhini <42486225+jkyazhini@users.noreply.github.com>
2021-04-23 11:37:29 +0530

    Create Cabtype.java

---
+++ Cabtype.java
@@ -0,0 +1,10 @@
+package com.yazhini;
+
+public class CabType {
+	public static void CAB() {
+		System.out.println("AVAILABLE CABS : \n 1.MICRO (Rs.10/km) \n 2.MINI (Rs.15/km) \n 3.PRIME (Rs.20/km) ");
+                              /**available cars in the cab rocker app      **/
+		PriceEstimator.paybill();      //call the next function
+	}
+
+}
